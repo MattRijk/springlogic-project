e3300131f671db5c7eddf59ffe70049d25aa38c1
Matt Rijk <matthewrijk@gmail.com>
2015-03-06 12:20:24 -0500

    configured beans

---
+++ src/main/java/com/natickweb/spring/Application.java
@@ -0,0 +1,20 @@
+package com.natickweb.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+		BeanA bean1 = context.getBean("bean1",BeanA.class);
+		BeanA bean2 = context.getBean("_beanA",BeanA.class);
+		
+		System.out.println(bean1.equals(bean2) );
+		
+		
+
+	}
+
+}
---
+++ src/main/java/com/natickweb/spring/BeanA.java
@@ -0,0 +1,11 @@
+package com.natickweb.spring;
+
+public class BeanA {
+
+	public BeanA() {
+		super();
+		System.out.println("Spring instatiated me");
+	}
+
+	
+}
